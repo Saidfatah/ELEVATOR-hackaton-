ab597052d02116c16d0475917938aceefa957555
Saidfatah <said_designer@outlook.com>
2023-07-22 14:45:16 +0100

    escaped ' with &apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <main >
       <div className='flex flex-col gap-8 justify-center items-center p-12' >
-        <h1>Bienvenue a l'appilication des joures</h1>
+        <h1>Bienvenue a l&apos;appilication des joures</h1>
         <Link href={"/players/1"} className=" flex items-center border border-black  p-2" >
           <span>Visualizer les list des jouers</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
